75ab8a268a07bd58d14c27f873a8502957ccfd97
Marcus Fermino <88506789+mf-linux@users.noreply.github.com>
2022-08-10 11:02:52 -0300

    Add files via upload
    Script de instalação do Docker

---
+++ docker-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Remove instalações anteriores
+pacotes='docker-ce docker-ce-cli containerd.io docker-compose-plugin'
+sudo yum rm -y $pacotes &>/dev/null
+
+# Prepara os requisitos para a instalação do docker
+echo "Preparando pré-requisitos"
+sudo yum in -y yum-utils &>/dev/null
+sudo yum in -y config-manager --enable extras &>/dev/null
+
+# Adicionando o repositório do Docker
+echo "Adicionando repositório..."
+sudo yum config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &>/dev/null
+
+# Instalando
+echo "Instalando Docker e suas dependências"
+sudo yum in -y $pacotes &>/dev/null
+
+
