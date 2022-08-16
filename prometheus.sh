df5a59ba14db8453f118e1e31bdcac709504e125
Marcus Fermino <marcusfermino.linux@gmail.com>
2022-08-16 20:09:02 +0000

    Script de instalação do prometheus e do alertmanager

---
+++ prometheus.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+prom_link='https://github.com/prometheus/prometheus/releases/download/v2.38.0/prometheus-2.38.0.linux-amd64.tar.gz'
+am_link='https://github.com/prometheus/alertmanager/releases/download/v0.24.0/alertmanager-0.24.0.linux-amd64.tar.gz'
+
+echo "Instalando o wget"
+sudo yum in -y wget &>/dev/null
+
+echo "Obtendo os arquivos para a instalação do Prometheus"
+wget $prom_link
+echo "Obtendo os arquivos para a instalação do Alert Manager"
+wget $am_link
+
+echo "Extraindo os arquivos necessários"
+tar -xzf prometheus-2.38.0.linux-amd64.tar.gz
+tar -xzf alertmanager-0.24.0.linux-amd64.tar.gz
+
+echo "Copiando os diretórios para  /usr/local/lib"
+sudo mv prometheus-2.38.0.linux-amd64 /usr/local/lib/
+sudo mv alertmanager-0.24.0.linux-amd64 /usr/local/lib/
+
+echo "Ajustando os executáveis.."
+cd /usr/local/bin
+sudo ln -sT /usr/local/lib/prometheus prometheus
+sudo ln -sT /usr/local/lib/promtool promtool
+sudo ln -sT /usr/local/lib/alertmanager alertmanager
+sudo ln -sT /usr/local/lib/amtool amtool
+
+echo "Finalizado"
+
+
