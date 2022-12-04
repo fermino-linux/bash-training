a6153e37e40938f9e43930afa2a77ad26db24cc9
Fermino <marcusfermino.linux@gmail.com>
2022-12-04 19:00:21 -0300

    Alterações

--- prometheus/install.sh
+++ prometheus/install.sh
@@ -9,55 +9,77 @@
 # Exemplos:
 #   Instala o prometheus
 #       ./install.sh 
+#
+#
+#
+# Variáveis
+SCRIPT_WORKING_DIR=/tmp
+
+PROMETHEUS_CONFIG_DIR=/etc/prometheus
+PROMETHEUS_CONSOLE_TEMPLATES=${PROMETHEUS_CONFIG_DIR}/console
+PROMETHEUS_CONSOLE_LIBRARIES=${PROMETHEUS_CONFIG_DIR}/console_libraries
+PROMETHEUS_DATA_DIR=/var/lib/prometheus
+PROMETHEUS_LOG_DIR=${PROMETHEUS_DATA_DIR}/logs
+
+PROMETHEUS_CONFIG_FILE=${PROMETHEUS_CONFIG_DIR}/prometheus.yml
+PROMETHEUS_SERVERLOG_FILE=${PROMETHEUS_LOG_DIR}/serverlog
+PROMETHEUS_ERRORLOG_FILE=${PROMETHEUS_LOG_DIR}/errorlog
+#
+#
+#
+#
+# Funções
+user_def() {
+  # Prepara usuário do prom
+  groupadd -r prometheus
+  useradd -d $PROMETHEUS_DATA_DIR -r --shell=/usr/sbin/nologin -g prometheus prometheus
+}
+
+create_dir() {
+  # Cria a infraestrutura de diretórios do prom
+  mkdir $PROMETHEUS_CONFIG_DIR $PROMETHEUS_LOG_DIR
+  
+  chown prometheus:prometheus $PROMETHEUS_CONFIG_DIR
+  chown prometheus:prometheus $PROMETHEUS_LOG_DIR
+
+  chmod 775 $PROMETHEUS_CONFIG_DIR
+  chmod 775 $PROMETHEUS_LOG_DIR
+}
+
+get_prom() {
+  # Faz o download do tarball do prometheus
+  # e extrai seus arquivos para os diretórios
+  # correspondentes.
+  local url='https://github.com/prometheus/prometheus/releases/download/v2.40.3/prometheus-2.40.3.linux-amd64.tar.gz'
+  local filename='prometheus.tar.gz'
+
+  curl -fsSLo /tmp/$filename $url
+  mkdir /tmp/output && tar -C /tmp/output --strip-components=1 -xf $filename
+
+  chown -R prometheus:prometheus /tmp/output/*
+
+  mv /tmp/output/{consoles, console_libraries, prometheus.yml} $PROMETHEUS_CONFIG_DIR
+  mv /tmp/output/{prometheus,promtool} /usr/sbin/
+}
+
+generate_promctl() {
+  # Cria o script promctl e o deixa pronto pra uso
+
+}
+
+create_service() {
+  # Cria o serviço do prometheus
+} 
+#
+#
+# Execução
 
 set -eo pipefail
 
-## Obtem o prometheus
-prom_url='https://github.com/prometheus/prometheus/releases/download/v2.40.3/prometheus-2.40.3.linux-amd64.tar.gz'
-prom_filename='prometheus.tar.gz'
+user_def 
+create_dir 
+get_prom
 
-curl -fsSLo $prom_filename $prom_url && mkdir prometheus 
-
-tar -C prometheus --strip-components 1 -xf $prom_filename
-
-rm -f $prom_filename
-
-# Cria os diretórios para gestão do prometheus
-sudo mkdir /var/lib/prometheus # para armazenar dados
-sudo mkdir /etc/prometheus
-
-# move os arquivos 
-sudo mv prometheus/{consoles,console_libraries,prometheus.yml} /etc/prometheus
-sudo mv prometheus/{prometheus,promtool} /usr/sbin
-
-# Cria a o serviço do prometheus
-cat << EOF | sudo tee > /etc/systemd/system/prometheus.service
-[Unit]
-Description=Prometheus
-Documentation=https://prometheus.io/docs/introduction
-Wants=network-online.target
-After=network-online.target
-
-[Service]
-Type=simple
-User=root
-Group=root
-ExecStart=/usr/sbin/prometheus \\
-  --config.file=/etc/prometheus/prometheus.yml \\
-  --storage.tsdb.path=/var/lib/prometheus \\
-  --web.console.templates=/etc/prometheus/consoles \\
-  --web.console.libraries=/etc/prometheus/consoles_libraries
-ExecReload=/usr/bin/kill -s SIGHUP "\$MAINPID"
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
-# Reinicia o systemd e inicializa o prometheus
-sudo systemctl daemon-reload
-sudo systemctl enable --now prometheus
 
 
 
